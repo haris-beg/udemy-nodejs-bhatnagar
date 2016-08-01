f26072807b65742eb89b0dba934c694f38bd059a
Haris Beg <haris.beg@gmail.com>
2016-07-31 20:27:28 -0700

    BasicRouter.js - 4th iteration

--- BasicRouter.js
+++ BasicRouter.js
@@ -2,11 +2,42 @@
 const http = require('http');
 const url = require('url');
 
+let routes = {
+	'GET': {
+		'/': (req, res) => {
+			res.writeHead(200, {'Content-type': 'text/html'});
+			res.end('<h1>Hello Router!</h1>');
+		},
+		'/about': (req, res) => {
+			res.writeHead(200, {'Content-type': 'text/html'});
+			res.end('<h1>This is the about page!</h1>');
+		},
+		'/api/getinfo': (req, res) => {
+			// In a more sophisticated app, this is where you would
+			// fetch data from a DB and respond as JSON
+			res.writeHead(200, {'Content-type': 'text/html'});
+			res.end(JSON.stringify(req.queryParams));
+		}
+	},
+	'POST': {
+
+	},
+	'NA': (req, res) => {
+		res.writeHead(404);
+		res.end('Content not found!');
+	}
+}
+
 function router(req, res) {
 	let baseURI = url.parse(req.url, true);
-	console.log('Requested route is: ', baseURI);
-	res.writeHead(200, {'Content-type': 'text/html'});
-	res.end('<h1>Hello Router!</h1>');
+	let resolveRoute = routes[req.method][baseURI.pathname];
+	if (resolveRoute != undefined) {
+		req.queryParams = baseURI.query;
+		resolveRoute(req, res);
+	}
+	else {
+		routes['NA'](req, res);
+	}
 }
 
 http.createServer(router).listen(3000, () => {
