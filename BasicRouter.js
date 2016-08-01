e80e1eaf9a6fdbf0080dec08ee3a493f806bcbf2
Haris Beg <haris.beg@gmail.com>
2016-07-31 19:53:12 -0700

    BasicRouter.js - 2nd iteration

--- BasicRouter.js
+++ BasicRouter.js
@@ -2,6 +2,8 @@
 const http = require('http');
 
 function router(req, res) {
+	console.log('Requested route is: ', req.url);
+	console.log('Request method is: ', req.method);
 	res.writeHead(200, {'Content-type': 'text/html'});
 	res.end('<h1>Hello Router!</h1>');
 }
