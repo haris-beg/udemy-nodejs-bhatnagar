d37e1db5ba2c9da41e57efa900b3a5b8a53b5f36
Haris Beg <haris.beg@gmail.com>
2016-08-16 17:12:18 -0700

    Basic route handling and listening to ports in Express

--- chatcat/server.js
+++ chatcat/server.js
@@ -1,3 +1,17 @@
 'use strict';
+const express = require('express');
+const app = express(); // creates an instance of an express app
 
-console.log("Welcome to ChatCat!");
+// creating a route handler for the root route
+app.get('/', (req, res, next) => {
+	res.send('<h1>Hello Express!</h1>');
+});
+
+app.get('/dashboard', (req, res, next) => {
+	res.send('<h1>This is the dashboard!');
+});
+
+// listen in on a port
+app.listen(3000, () => {
+	console.log('ChatCAT running on port: ', 3000);
+});
