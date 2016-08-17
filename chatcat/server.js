23b3c35c89db4afc3a34a0a5592fa584112bc8c6
Haris Beg <haris.beg@gmail.com>
2016-08-16 17:25:36 -0700

    Using Middleware

--- chatcat/server.js
+++ chatcat/server.js
@@ -4,13 +4,22 @@ const app = express(); // creates an instance of an express app
 
 app.set('port', process.env.PORT || 3000);
 
+let helloMiddleware = (req, res, next) => {
+	req.hello = "Hello! This is Haris running my first middleware function!!";
+	next();
+}
+
+// plug in the middleware
+app.use(helloMiddleware);
+
 // creating a route handler for the root route
 app.get('/', (req, res, next) => {
 	res.send('<h1>Hello Express!</h1>');
+	console.log(req.hello);
 });
 
 app.get('/dashboard', (req, res, next) => {
-	res.send('<h1>This is the dashboard!');
+	res.send('<h1>This is the dashboard! Middleware says: ' + req.hello + '</h1>');
 });
 
 // listen in on a port
