8786117ebcc409af98a4c9393e31a34881d42be4
Haris Beg <haris.beg@gmail.com>
2016-08-22 21:18:51 -0700

    Using EJS templating engine

--- chatcat/server.js
+++ chatcat/server.js
@@ -3,11 +3,15 @@ const express = require('express');
 const app = express(); // creates an instance of an express app
 
 app.set('port', process.env.PORT || 3000);
+app.set('views', './views');// not necessary, as it is same as default 
+app.set('view engine', 'ejs');
 
 // creating a route handler for the root route
 app.get('/', (req, res, next) => {
 	//res.send('<h1>Hello Express!</h1>');
-	res.sendFile(__dirname + '/views/login.htm');
+	res.render('login', {
+		pageTitle: 'My Login Page'
+	});
 });
 
 
