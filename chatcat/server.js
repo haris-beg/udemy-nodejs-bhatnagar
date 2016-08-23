d7aec396871103aa4923a99b53078d47ac378d84
Haris Beg <haris.beg@gmail.com>
2016-08-22 20:58:41 -0700

    Serving a static file

--- chatcat/server.js
+++ chatcat/server.js
@@ -4,23 +4,12 @@ const app = express(); // creates an instance of an express app
 
 app.set('port', process.env.PORT || 3000);
 
-let helloMiddleware = (req, res, next) => {
-	req.hello = "Hello! This is Haris running my first middleware function!!";
-	next();
-}
-
-// plug in the middleware
-app.use(helloMiddleware);
-
 // creating a route handler for the root route
 app.get('/', (req, res, next) => {
-	res.send('<h1>Hello Express!</h1>');
-	console.log(req.hello);
+	//res.send('<h1>Hello Express!</h1>');
+	res.sendFile(__dirname + '/views/login.htm');
 });
 
-app.get('/dashboard', (req, res, next) => {
-	res.send('<h1>This is the dashboard! Middleware says: ' + req.hello + '</h1>');
-});
 
 // listen in on a port
 app.listen(app.get('port'), () => {
