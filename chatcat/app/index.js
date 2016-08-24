9555906a9301050a7de3a5ca35e5c329c63ac0b9
Haris Beg <haris.beg@gmail.com>
2016-08-23 18:14:04 -0700

    Created Recursive function "registerRoutes" to routes/index.js to register routes in express

--- chatcat/app/index.js
+++ chatcat/app/index.js
@@ -1,14 +1,5 @@
 'use strict';
 
-const router = require('express').Router();
-
-// creating a route handler for the root route
-router.get('/', (req, res, next) => {
-	res.render('login', {
-        pageTitle: 'My Login Page'
-    });
-});
-
 module.exports = {
-    router: router
+    router: require('./routes')()
 }
