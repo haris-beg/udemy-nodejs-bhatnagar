d237ccaaac4190616deb84120a371c2835e12ddc
Haris Beg <haris.beg@gmail.com>
2016-08-29 15:13:58 -0700

    Ignoring some files

--- myModule/app.js
+++ myModule/app.js
@@ -5,6 +5,8 @@ const eng = new Enigma('harisbeg-key');
 
 let encodedString = eng.encode("Don't worry, be happy!");
 let decodedString = eng.decode(encodedString);
+let qr = eng.qrgen("http://www.npmjs.com", "outImage.png");
+qr ? console.log("Success: QR Code Image created!") : console.log("Error: QR Code failed");
 
 console.log('Encoded version of String = ' + encodedString);
 console.log('Decoded version of String = ' + decodedString);
--- myModule/enigma/index.js
+++ myModule/enigma/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 const crypto = require('crypto');
+const qr = require('qr-image');
+const fs = require('fs');
 
 module.exports = function(key) {
 	this.key = key;
@@ -12,6 +14,20 @@ module.exports = function(key) {
 		decode: (str) => {
 			let decoder = crypto.createDecipher('aes-256-ctr', this.key);
 			return decoder.update(str, 'hex', 'utf-8');
+		},
+		qrgen: (data, file) => {
+			let dataToEncode = data || null;
+			let outImage = file || null;
+			if(dataToEncode != null && outImage != null) {
+				qr.image(dataToEncode, {
+					type: 'png',
+					size: 20
+				}).pipe(fs.createWriteStream(outImage));
+				return true;
+			}
+			else {
+				return false;
+			}
 		}
 	}
 }
